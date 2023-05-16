7b771d0e27f95230289c287481d324e5936f6b82
Jack Bass <jtb01584@uga.edu>
2023-05-16 18:13:37 -0400

    Fixed push logic, added pop logic through much head-scratching and trial and error (some links may still be inaccurate)

--- src/cdl_list.rs
+++ src/cdl_list.rs
@@ -1,6 +1,6 @@
 //
 
-use std::{cell::{RefCell, Ref}, rc::{Rc, Weak}, fmt::{Debug, format}, borrow::{BorrowMut, Borrow}, ops::Deref};
+use std::{cell::{RefCell, Ref, RefMut}, rc::{Rc, Weak}, fmt::{Debug, format}, borrow::{BorrowMut, Borrow}, ops::Deref};
 
 #[derive(Debug)]
 enum LinkType<T> {
@@ -31,12 +31,6 @@ impl<T: Debug> Node<T> {
             prev: None, 
             data: t
         }
-    }
-}
-
-impl<T: Debug> std::ops::Drop for Node<T> {
-    fn drop(&mut self) {
-        println!("Dropping: {:?}", self);
     }
 }
 
@@ -93,11 +87,19 @@ impl<T: Debug> CdlList<T> {
 
             // adjust list head/tail
             self.head = Some(Rc::clone(&ref_n));
+
+            // save ourselves the headache later?
             self.tail = Some(Rc::clone(&ref_n));
+            // self.tail = Some(ref_n);
+            // self.tail = None;
         } else {
+            let head_ref = Rc::clone(&self.head.as_ref().unwrap());
+            let tail_ref = Rc::clone(&self.tail.as_ref().unwrap());
+
             // node->prev = current tail always
-            let tail_ref = Rc::downgrade(&self.tail.as_ref().unwrap());
-            ref_n_mut.prev = Some(LinkType::WeakLink(tail_ref));
+            // let weak_tail = Rc::downgrade(&self.tail.as_ref().unwrap());
+            let weak_tail = Rc::downgrade(&tail_ref);
+            ref_n_mut.prev = Some(LinkType::WeakLink(weak_tail));
 
             // Break and add node links based on insertion point: 
             // 1. node->next is either a strong or weak link to current head
@@ -105,30 +107,186 @@ impl<T: Debug> CdlList<T> {
             // 3. change head/tail to point to new node
             match insert_front {
                 true => {
-                    let head_ref = Rc::clone(&self.head.as_ref().unwrap());
-                    ref_n_mut.next = Some(LinkType::StrongLink(head_ref));
+                    // let head_ref = self.head.take().unwrap();
+                    ref_n_mut.next = Some(LinkType::StrongLink(head_ref.clone()));
 
-                    let head_ref = Rc::clone(&self.head.as_ref().unwrap());
+                    // let head_ref = Rc::clone(&self.head.as_ref().unwrap());
                     let mut head_ref_mut = head_ref.as_ref().borrow_mut();
                     let weak_n = Rc::downgrade(&ref_n);
                     head_ref_mut.prev = Some(LinkType::WeakLink(weak_n));
+                    
+                    if self.size() == 1 {
+                        //head->next points to self right now, need to fix
+                        let weak_n = Rc::downgrade(&ref_n);
+                        head_ref_mut.next = Some(LinkType::WeakLink(weak_n));
+                    }
 
+                    let _ = self.head.take();
                     self.head = Some(ref_n);
+                    // self.head = Some(Rc::clone(&ref_n));
                 }, 
                 false => {
-                    let head_ref = Rc::downgrade(&self.head.as_ref().unwrap());
-                    ref_n_mut.next = Some(LinkType::WeakLink(head_ref));
+                    // let head_ref = Rc::downgrade(&self.head.as_ref().unwrap());
+                    let weak_head = Rc::downgrade(&head_ref);
+                    ref_n_mut.next = Some(LinkType::WeakLink(weak_head));
 
-                    let tail_ref = Rc::clone(&self.tail.as_ref().unwrap());
+                    // let tail_ref = Rc::clone(&self.tail.as_ref().unwrap());
+                    // let tail_ref = self.tail.take().unwrap();
                     let mut tail_ref_mut = tail_ref.as_ref().borrow_mut();
-                    let weak_n = Rc::downgrade(&ref_n);
-                    tail_ref_mut.next = Some(LinkType::WeakLink(weak_n));
+                    // let weak_n = Rc::downgrade(&ref_n);
+                    tail_ref_mut.next = Some(LinkType::StrongLink(Rc::clone(&ref_n)));
 
+                    if self.size == 1 {
+                        //tail->prev = tail, which is wrong
+                        let weak_n = Rc::downgrade(&ref_n);
+                        tail_ref_mut.prev = Some(LinkType::WeakLink(weak_n));
+                    }
+
+                    let _ = self.tail.take();
                     self.tail = Some(ref_n);
                 }
             };
         }
 
         self.size += 1;
+    }
+
+    //only for testing
+    pub fn print_strong_counts(&self) {
+        let head_ref = Rc::clone(&self.head.as_ref().unwrap());
+        let tail_ref = Rc::clone(&self.tail.as_ref().unwrap());
+
+        if self.size == 1 {
+            println!("SC:\n\tHead: {}\n\tTail: {}\n\n\n", Rc::strong_count(&head_ref)-2, Rc::strong_count(&tail_ref)-2);
+        } else {
+            println!("SC:\n\tHead: {}\n\tTail: {}\n\n\n", Rc::strong_count(&head_ref)-1, Rc::strong_count(&tail_ref)-1);
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.pop(true)
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.pop(false)
+    }
+
+    fn pop(&mut self, pop_front: bool) -> Option<T> {
+        if self.is_empty() {
+            return None;
+        }
+
+        self.size -= 1;
+
+        if self.size == 0 {
+            let head = self.head.take().unwrap();
+            drop(head);
+            let tail = self.tail.take().unwrap();
+            let val = Rc::try_unwrap(tail).ok().unwrap().into_inner().data;
+            return Some(val);
+        } else {
+            match pop_front {
+                true => {
+                    //pop head
+                    let head = self.head.take().unwrap();
+                    let node = Rc::try_unwrap(head).ok().unwrap().into_inner();
+                    let next = node.next.unwrap();
+
+                    match next {
+                        LinkType::StrongLink(sl) => {
+                            // fix links: 
+                            // 1. next->prev = tail
+                            // 2. tail->next = next
+                            {
+                                let tail_ref = Rc::clone(&self.tail.as_ref().unwrap());
+                                let mut tail_ref_mut = tail_ref.as_ref().borrow_mut();
+
+                                // let sl2 = Rc::clone(&sl);
+                                let weak_ref = Rc::downgrade(&sl);
+
+                                tail_ref_mut.next = Some(LinkType::WeakLink(weak_ref));
+                            }
+
+                            {
+                                let tail_ref = Rc::clone(&self.tail.as_ref().unwrap());
+                                let mut next_ref_mut = sl.as_ref().borrow_mut();
+                                let weak_tail = Rc::downgrade(&tail_ref);
+
+                                next_ref_mut.prev = Some(LinkType::WeakLink(weak_tail));
+                            }
+                            
+
+                            self.head = Some(sl);
+                        }, 
+                        _ => panic!("I'd like to see you reach this")
+                    }
+
+                    return Some(node.data);
+                }, 
+                false => {
+                    //pop tail
+                    // By design, tail will have more strong links than head
+                    // ==> [H] <==> ... <==> [T] <==
+                    // ||                         ||
+                    // =============================
+                    // Tail->next = head is weak, so strong_count(&head) = 1
+                    // Strong tail links are tail and tail->prev->next   = 2
+                    //
+                    // Therefore, must break tail->prev->next link before consuming tail
+                    {
+                        let tail_ref = Rc::clone(&self.tail.as_ref().unwrap());
+                        let tail_borrow = tail_ref.as_ref().borrow();
+                        let p = tail_borrow.prev.clone().unwrap();
+                        match p {
+                            LinkType::WeakLink(wl) => {
+                                let up = Weak::upgrade(&wl).unwrap();
+                                let mut up_ref_mut = up.as_ref().borrow_mut();
+                                let weak_head = Rc::downgrade(&self.head.as_ref().unwrap());
+
+                                let _ = up_ref_mut.next.take();
+                                up_ref_mut.next = Some(LinkType::WeakLink(weak_head));
+                            }, 
+                            _ => panic!("Nice try")
+                        };
+                    }
+
+                    let tail = self.tail.take().unwrap();
+
+                    let node = Rc::try_unwrap(tail).ok().unwrap().into_inner();
+                    let prev = node.prev.unwrap();
+
+                    match prev {
+                        LinkType::WeakLink(wl) => {
+                            // fix links: 
+                            // 1. prev->next = head
+                            // 2. head->prev = prev
+                            
+                            {
+                                // Upgrade prev to strong link
+                                let strong_ref = Weak::upgrade(&wl).unwrap();
+                                let mut strong_ref_mut = strong_ref.as_ref().borrow_mut();
+                                let weak_head = Rc::downgrade(&self.head.as_ref().unwrap());
+
+                                strong_ref_mut.next = Some(LinkType::WeakLink(weak_head));
+                            }
+
+                            {
+                                let head_ref = Rc::clone(&self.head.as_ref().unwrap());
+                                let mut head_ref_mut = head_ref.as_ref().borrow_mut();
+
+                                let wl2 = Weak::clone(&wl);
+                                head_ref_mut.prev = Some(LinkType::WeakLink(wl2));
+                            }
+
+                            let strong_ref = Weak::upgrade(&wl).unwrap();
+                            self.tail = Some(strong_ref);
+                        }, 
+                        _ => panic!("I reiterate.")
+                    }
+
+                    return Some(node.data);
+                }
+            }
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,6 @@ mod tests {
     use cdl_list::CdlList;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
     fn test_cdl_empty() {
         let list : CdlList<u32> = CdlList::new();
         assert!(list.is_empty());
@@ -23,7 +17,7 @@ mod tests {
         list.push_front(3);
         assert!(!list.is_empty());
 
-        println!("TEST: {:?}", list);
+        // println!("TEST: {:?}", list);
     }
 
     #[test]
@@ -32,15 +26,128 @@ mod tests {
         list.push_front(3);
         assert!(!list.is_empty());
 
-        println!("TEST 1: {:?}", list);
+        // println!("TEST 1: {:?}", list);
 
         list.push_front(4);
         assert!(!list.is_empty());
 
-        println!("TEST 2: {:?}", list);
+        // println!("TEST 2: {:?}", list);
 
         list.push_back(6);
 
-        println!("TEST 3: {:?}", list);
+        // println!("TEST 3: {:?}", list);
+    }
+
+    #[test]
+    fn test_empty_pop() {
+        let mut list : CdlList<u32> = CdlList::new();
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_one_pop() {
+        let mut list : CdlList<u32> = CdlList::new();
+        list.push_front(42);
+        let val = list.pop_front();
+
+        assert!(val.is_some());
+        assert_eq!(val, Some(42));
+        assert!(list.is_empty());
+    }
+
+    #[test]
+    fn test_pop_front() {
+        println!("\n\n\nTEST POP FRONT\n\n\n");
+
+        let mut list : CdlList<u32> = CdlList::new();
+        list.push_back(4);
+        list.push_back(8);
+        list.push_back(15);
+        list.push_back(16);
+        list.push_back(23);
+        list.push_back(42);
+
+        let val = list.pop_front();
+        assert!(val.is_some());
+        assert_eq!(val, Some(4));
+        // println!("VAL: {:?}", val);
+        assert_eq!(list.size(), 5);
+
+        let val = list.pop_front();
+        assert!(val.is_some());
+        // println!("VAL: {:?}", val);
+        assert_eq!(val, Some(8));
+        assert_eq!(list.size(), 4);
+
+        let val = list.pop_front();
+        assert!(val.is_some());
+        // println!("VAL: {:?}", val);
+        assert_eq!(val, Some(15));
+        assert_eq!(list.size(), 3);
+
+        let val = list.pop_front();
+        assert!(val.is_some());
+        // println!("VAL: {:?}", val);
+        assert_eq!(val, Some(16));
+        assert_eq!(list.size(), 2);
+
+        let val = list.pop_front();
+        assert!(val.is_some());
+        // println!("VAL: {:?}", val);
+        assert_eq!(val, Some(23));
+        assert_eq!(list.size(), 1);
+
+        let val = list.pop_front();
+        assert!(val.is_some());
+        // println!("VAL: {:?}", val);
+        assert_eq!(val, Some(42));
+        assert!(list.is_empty());
+    }
+
+    #[test]
+    fn test_pop_back() {
+        let mut list : CdlList<u32> = CdlList::new();
+        list.push_front(4);
+        list.push_front(8);
+        list.push_front(15);
+        list.push_front(16);
+        list.push_front(23);
+        list.push_front(42);
+
+        let val = list.pop_back();
+        assert!(val.is_some());
+        assert_eq!(val, Some(4));
+        println!("VAL 2: {:?}", val);
+        assert_eq!(list.size(), 5);
+
+        let val = list.pop_back();
+        assert!(val.is_some());
+        assert_eq!(val, Some(8));
+        println!("VAL 2: {:?}", val);
+        assert_eq!(list.size(), 4);
+
+        let val = list.pop_back();
+        assert!(val.is_some());
+        assert_eq!(val, Some(15));
+        println!("VAL 2: {:?}", val);
+        assert_eq!(list.size(), 3);
+
+        let val = list.pop_back();
+        assert!(val.is_some());
+        assert_eq!(val, Some(16));
+        println!("VAL 2: {:?}", val);
+        assert_eq!(list.size(), 2);
+
+        let val = list.pop_back();
+        assert!(val.is_some());
+        assert_eq!(val, Some(23));
+        println!("VAL 2: {:?}", val);
+        assert_eq!(list.size(), 1);
+
+        let val = list.pop_back();
+        assert!(val.is_some());
+        assert_eq!(val, Some(42));
+        println!("VAL 2: {:?}", val);
+        assert_eq!(list.size(), 0);
     }
 }
